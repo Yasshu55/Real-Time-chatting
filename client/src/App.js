9cb745ef33952d3263dc67d1c76f0d9333a823a8
Yashwanth sai <20r91a0530@tkrec.ac.in>
2023-03-30 17:53:44 +0530

    client and server folders

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import React,{useState,createContext} from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Nav from './Nav';
+import Register from './Register';
+import Login from './Login';
+import Myprofile from './Myprofile';
+
+export const store = createContext();
+
+const App = () => {
+ const [token,setToken] = useState(null);
+
+  return (
+    <div>
+    <store.Provider value={[token,setToken]}>
+        <Nav />
+        <Router>
+          <Routes>
+             <Route path='/register' element={<Register/>} />
+             <Route path='/login' element={<Login/>} />
+             <Route path='/myprofile' element={<Myprofile/>}></Route>
+          </Routes>
+        </Router>
+    </store.Provider>
+
+
+    </div>
+  )
+}
+
+export default App
